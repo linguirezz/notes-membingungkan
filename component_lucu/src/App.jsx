25e1fbec4cceb0643b390f13d1ed261237a3c2e4
linguirezz <165681231+linguirezz@users.noreply.github.com>
2024-04-06 17:52:23 +0700

    penjelasan logic dari feature baru

--- component_lucu/src/App.jsx
+++ component_lucu/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 
 function App() {
   const [elements, setElements] = useState([]);
-
+// add logic =>membuat salinan dari hooks yang membuat hooks tetap kosong jadi tombol add hanya akan menambahkan satu element perclick
   const addElement = () => {
     setElements(prevElements => [...prevElements, 'Elemen Baru']);
   }
-
+// delete logic => menghasilkan list baru yang kosong karena tidak ada kondisi yang sesuai dengan method fi;ter
   const deleteElement = (index) => {
     setElements(prevElements => prevElements.filter((_, i) => i !== index));
   }
